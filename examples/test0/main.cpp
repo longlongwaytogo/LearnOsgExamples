1829e897c9b28fc03d74a1fdbfd7c1a5a2f7a99e
szh <szh@test.com>
2018-03-16 16:53:58 +0800

    create osg example project base on cmake

---
+++ examples/test0/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <osgViewer/viewer>
+#include <osg/Node>
+#include <osgDB/ReadFile>
+int main()
+{
+	osgViewer::Viewer viewer;
+	osg::Node* pNode = osgDB::readNodeFile("cow.osg");
+	if(pNode)
+	{
+
+		viewer.setSceneData(pNode);
+	}
+	viewer.realize();
+	viewer.run();
+	return 0;
+}
