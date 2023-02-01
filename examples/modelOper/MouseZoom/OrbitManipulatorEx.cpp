ed0b1e9a541102ae80e86216163f98c969378b91
longlongwaytogo <longlongwaytogo@users.noreply.github.com>
2023-02-01 11:54:55 +0800

    modify

--- examples/modelOper/MouseZoom/OrbitManipulatorEx.cpp
+++ examples/modelOper/MouseZoom/OrbitManipulatorEx.cpp
@@ -7,6 +7,8 @@
 #include <math.h>
 #include <float.h>
 
+using namespace osgGA;
+
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
 #endif
@@ -14,48 +16,12 @@
 #ifndef min
 #define min(a,b)            (((a) < (b)) ? (a) : (b))
 #endif
-#define USE_PIXEL_COMPUTE_DISTANCE 0
-#define MOVEUSETIME 2000
-#define PRECISION_LOW 1.0e-4
-#define PRECISION_HIG 1.0e7
-
-using namespace osgGA;
 
 double calOrthNearFar(double w, double h)
 {
 	double dw = fabs(w);
 	double dh = fabs(h);
 	return max(min(dw, dh) * 100.0, 4000.0);
-}
-osg::Vec3d screen2World(osg::Camera* pCamera, osg::Vec3d pos)
-{
-	osg::Matrixd mvpw = pCamera->getViewMatrix() * pCamera->getProjectionMatrix() * pCamera->getViewport()->computeWindowMatrix();
-	osg::Matrixd inv = mvpw.inverse(mvpw);
-	return pos * inv;
-}
-
-
-osg::Vec3d world2Screen(osg::Camera* pCamera, osg::Vec3d pos)
-{
-	osg::Matrixd mvpw = pCamera->getViewMatrix() * pCamera->getProjectionMatrix() * pCamera->getViewport()->computeWindowMatrix();
-	return pos * mvpw;
-}
-
-osg::Vec3d screen2View(osg::Camera* pCamera, osg::Vec3d pos)
-{
-	osg::Matrix screenMat = pCamera->getViewport()->computeWindowMatrix();
-	osg::Vec4d d2 = osg::Vec4d(pos.x(), pos.y(), pos.z(), 1.0) * osg::Matrix::inverse(screenMat);
-	osg::Matrix mvpw =  pCamera->getProjectionMatrix() * pCamera->getViewport()->computeWindowMatrix();
-	osg::Matrix inv = mvpw.inverse(mvpw);
-	osg::Vec4d dd = osg::Vec4d(pos.x(), pos.y(), pos.z(), 1.0) * inv;
-	dd = dd / dd.w();
-	return pos * inv;
-}
-
-osg::Vec3d View2Screen(osg::Camera* pCamera, osg::Vec3d pos)
-{
-	osg::Matrix mvpw =pCamera->getProjectionMatrix() * pCamera->getViewport()->computeWindowMatrix();
-	return pos * mvpw;
 }
 
 
@@ -72,11 +38,7 @@ int floatComp(double a, double b, double epson = 1.0e-6)
  
 float OrbitManipulatorEx::_fov = 45.0f;
 float OrbitManipulatorEx::_zNear = 0.25f;
-
-osgGA::GUIEventAdapter* _ea = nullptr;
-osgGA::GUIActionAdapter* _us = nullptr;
  
-
 bool OrbitManipulatorEx::ZoomPerspective(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us)
 {
 
@@ -250,247 +212,16 @@ bool OrbitManipulatorEx::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActi
 	default:
 		return false;
 	}
-
-	
  
 	return false;
 }
 
-bool OrbitManipulatorEx::handleMouseWheel(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us)
+
+OrbitManipulatorEx::OrbitManipulatorEx()
 {
-	osgGA::GUIEventAdapter::ScrollingMotion sm = ea.getScrollingMotion();
-	 
-	// handle centering
-	if (_flags & SET_CENTER_ON_WHEEL_FORWARD_MOVEMENT)
-	{
-
-		if (((sm == osgGA::GUIEventAdapter::SCROLL_DOWN && _wheelZoomFactor > 0.)) ||
-			((sm == osgGA::GUIEventAdapter::SCROLL_UP && _wheelZoomFactor < 0.)))
-		{
-
-			if (getAnimationTime() <= 0.)
-			{
-				// center by mouse intersection (no animation)
-				setCenterByMousePointerIntersection(ea, us);
-			}
-			else
-			{
-				// start new animation only if there is no animation in progress
-				if (!isAnimating())
-					startAnimationByMousePointerIntersection(ea, us);
-
-			}
-
-		}
-	}
-
-	//osg::Camera* pCamera = us.asView()->getCamera();
-	//if (isOrthoCamera(pCamera->getProjectionMatrix()))
-	//{
-	//	return onZoomOrtho(ea, us);
-	//}
-	//else
-	//{
-	//	//return onZoomPerspective(*_ea, *_us);
-	//	if(sm ==  osgGA::GUIEventAdapter::SCROLL_UP)
-	//		return onZoomPerspective(*_ea, *_us, -_wheelZoomFactor, true);
-	//	else if (sm == osgGA::GUIEventAdapter::SCROLL_DOWN)
-	//		return onZoomPerspective(*_ea, *_us, _wheelZoomFactor, true);
-	//}
-	switch (sm)
-	{
-		// mouse scroll up event
-	case osgGA::GUIEventAdapter::SCROLL_UP:
-	{
-		// perform zoom
-		zoomModel(-_wheelZoomFactor, true);
-		us.requestRedraw();
-		us.requestContinuousUpdate(isAnimating() || _thrown);
-		return true;
-	}
-
-	// mouse scroll down event
-	case osgGA::GUIEventAdapter::SCROLL_DOWN:
-	{
-		// perform zoom
-		zoomModel(_wheelZoomFactor, true);
-		us.requestRedraw();
-		us.requestContinuousUpdate(isAnimating() || _thrown);
-		return true;
-	}
-
-	// unhandled mouse scrolling motion
-	default:
-		return false;
-	}
-}
-
-bool OrbitManipulatorEx::onPan(const double eventTimeDelta, const double dx, const double dy)
-{
-	// pan model
-	float scale = 1.0;// -0.3f * _distance * getThrowScale(eventTimeDelta);
-	
-	panModel(dx * scale, dy * scale);
-	return true;
-
-}
-bool OrbitManipulatorEx::onZoom(const double eventTimeDelta, const double dx, const double dy)
-{
-	// zoom model
-	zoomModel(dy * getThrowScale(eventTimeDelta), true);
-	return true;
-}
-bool OrbitManipulatorEx::onRotate(const double eventTimeDelta, const double dx, const double dy)
-{
-	// rotate camera
-	if (getVerticalAxisFixed())
-		rotateWithFixedVertical(dx, dy);
-	else
-		rotateTrackball(_ga_t0->getXnormalized(), _ga_t0->getYnormalized(),
-			_ga_t1->getXnormalized(), _ga_t1->getYnormalized(),
-			getThrowScale(eventTimeDelta));
-
-	return true;
-}
- 
-inline bool OrbitManipulatorEx::onZoomOrtho(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us, double dy /*= 0.0f*/, bool offsetCenter/* = true*/)
-{
-	return true;
-}
-
-bool OrbitManipulatorEx::onZoomPerspective(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us, double dy, bool offsetCenter)
-{
-	{
-		double l, r, b, t, n, f;
-		_pCamera->getProjectionMatrixAsFrustum(l, r, b, t, n, f);
-		printf("frustum:%f,%f,%f,%f\n", l, r, b, t);
-	}
-	bool  pushForwardIfNeeded = true;
-	float scale = 1.0f + dy*1;
-	// 计算鼠标在屏幕占比例
-	double x = (ea.getX() - ea.getXmin()) / ea.getXmax();
-	double y = (ea.getY() - ea.getYmin()) / ea.getYmax();
-	// 从0，1---变换到-1--+1范围
-	x -= 0.5;
-	y -= 0.5;
-	x *= 2;
-	y *= 2;
-
-	osg::Camera* pCamera = us.asView()->getCamera();
-	osg::Vec3d eye, center, up;
-	getTransformation(eye, center, up);
-
-	auto ZoomPerspective = [this, &eye, &center, &up](osg::Camera* pCamera, double dx, double dy)-> void {
-		if (pCamera)
-		{
-			osg::Matrix m = pCamera->getProjectionMatrix();
-			{
-				double fovy = 0;
-				double aspectRatio = 0;
-				double zNear = 0;
-				double zFar = 0;
-				m.getPerspective(fovy, aspectRatio, zNear, zFar);
-				// 离地表的高度作为缩放控制因子
-				double dDistance = fabs(eye.z());
-				dDistance = osg::clampBetween<double>(dDistance, 1.0, 20000);
-				// 获取视线方向与up方向的夹角
-				osg::Vec3d vFoward = center - eye;
-				osg::Vec3d vFowardOrigin = vFoward;
-				vFoward.normalize();
-			
-				double speed = 1.0;// 0.5;
-				dDistance = _distance;
-				double dHeight = tan(osg::DegreesToRadians(fovy * 0.5)) * dDistance * speed;
-				dHeight = fabs(dHeight);
-				double dWidth = aspectRatio * dHeight;
-
-				osg::Vec3d vRight = vFoward ^ up;
-				vRight.normalize();
-				osg::Vec3d vUp = vRight ^ vFoward;
-				vUp.normalize();
-
-				eye -= vRight * dWidth * dx;
-				eye -= vUp * dHeight * dy;
-
-				center = eye + vFowardOrigin;
-
-				setTransformation(eye, center, up);
-			}
-		}
-	};
-
-	if (offsetCenter)
-	{
-		ZoomPerspective(pCamera, -x, -y);
-	}
-	// minimum distance
-	float minDist = _minimumDistance;
-	if (getRelativeFlag(_minimumDistanceFlagIndex))
-		minDist *= _modelSize;
-#if 1
-	if (_distance * scale > minDist)
-	{
-		// regular zoom
-		_distance *= scale;
-	}
-	else
-	{
-		pushForwardIfNeeded = true;
-		if (pushForwardIfNeeded)
-		{
-			// push the camera forward
-			float yscale = -_distance;
-			osg::Matrixd rotation_matrix(_rotation);
-			osg::Vec3d dv = (osg::Vec3d(0.0f, 0.0f, -1.0f) * rotation_matrix) * (dy * yscale);
-			_center += dv;
-		}
-		else
-		{
-			// set distance on its minimum value
-			_distance = minDist;
-		}
-	}
-
-#else
-	
-	{
-		float yscale = -10;
-		osg::Matrixd rotation_matrix(_rotation);
-		osg::Vec3d dv = (osg::Vec3d(0.0f, 0.0f, -1.0f) * rotation_matrix) * (dy * yscale);
-		_center += dv;
-		
-	}
-	//getTransformation(eye, center, up);
-	//osg::Vec3d forward = center - eye;
-	//float d = forward.length();
-	//forward *= dy;
-	//eye = eye + forward;
-	//center = center + forward;
-	//setTransformation(eye, center, up);
-#endif 
-	if (offsetCenter)
-	{
-		getTransformation(eye, center, up);
-		ZoomPerspective(pCamera, x, y);
-	}
-	printf("distance:%f,eye:%f,%f,%f\n", _distance,eye.x(),eye.y(),eye.z());
-	return true;
-}
-
-
-
-OrbitManipulatorEx::OrbitManipulatorEx(): 
-	_bFlyTo(false), 
-	_bLeftDown(false),
-	_lastFrame(-1)
-{
-	_allowThrow = false;
-	_lastPickedPos.set(0, 0, 0);
-	_lastPanMousePos.set(0, 0);
 	_pViewer = nullptr;
 	_pCamera = nullptr;
 }
- 
  
 
 bool OrbitManipulatorEx::isOrthoCamera(const osg::Matrix& m)
@@ -507,21 +238,7 @@ bool OrbitManipulatorEx::switchToOrtho(osg::Camera* pCamera)
 	const_cast<OrbitManipulatorEx*>(this)->getTransformation(_eye, _center, _up);
 	if (!isOrtho)
 	{
-		//OcGePoint3d eye = OcGePoint3d(_eye.x(), _eye.y(), _eye.z());
-		//OcGePoint3d center = OcGePoint3d(_center.x(), _center.y(), _center.z());
-		//OcGeLine3d line(eye, center);
-		//OcGeRay3d ray(eye, center);
-		//OcGePlane pln = OcGePlane::kXYPlane;
-		//OcGePoint3d intersectPt;
-		//if (line.intersectWith(pln, intersectPt))
-		//	//if(ray.intersectWith(pln,intersectPt))
-		//{
-		//	if (intersectPt != eye)// 交点如果不是起点
-		//	{
-		//		center = intersectPt;
-		//		_center = osg::Vec3d(center.x, center.y, center.z);
-		//	}
-		//}
+		
 		setTransformation(_eye, _center, _up);
 		double left, right, top, bottom, zNear, zFar;
 		p.getFrustum(left, right, bottom, top, zNear, zFar);
@@ -550,23 +267,6 @@ bool OrbitManipulatorEx::switchToPerspective(osg::Camera* pCamera)
 	const_cast<OrbitManipulatorEx*>(this)->getTransformation(_eye, _center, _up);
 	if (isOrtho)
 	{
-	/*	OcGePoint3d eye(_eye.x(), _eye.y(), _eye.z());
-		OcGePoint3d center(_center.x(), _center.y(), _center.z());
-		OcGeVector3d foward = center - eye;
-		foward.normalize();
-		OcGePoint3d intersectPt;
-		OcGePlane pln = OcGePlane::kXYPlane;
-		OcGeLine3d line(eye, center);
-		OcGeRay3d ray(eye, center);
-		if (line.intersectWith(pln, intersectPt))*/
-			//if (ray.intersectWith(pln, intersectPt))
-		//{
-		//	if (intersectPt != eye)// 交点如果不是起点
-		//	{
-		//		center = intersectPt;
-		//		_center = osg::Vec3d(center.x, center.y, center.z);
-		//	}
-		//}
 		setTransformation(_eye, _center, _up);
 		double left, right, top, bottom, zNear, zFar;
 		p.getOrtho(left, right, bottom, top, zNear, zFar);
--- examples/modelOper/MouseZoom/OrbitManipulatorEx.h
+++ examples/modelOper/MouseZoom/OrbitManipulatorEx.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "osgGA\TrackballManipulator"
- 
-
 #include <atomic>
 
 namespace osgViewer
@@ -9,68 +7,26 @@ namespace osgViewer
 	class Viewer;
 }
 
-
 class OrbitManipulatorEx :
     public osgGA::OrbitManipulator
 {
 	bool ZoomPerspective(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
 	bool ZoomOrtho(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
-	virtual bool handleMouseWheel(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
-	
-	
-	bool onPan(const double eventTimeDelta, const double dx, const double dy);
-	bool onZoom(const double eventTimeDelta, const double dx, const double dy);
-	bool onRotate(const double eventTimeDelta, const double dx, const double dy);
-	bool onRoaming(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us);
 
-
-	
-	bool onZoomOrtho(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us, double dy = 0.0, bool offsetCenter = true); // 偏移中心缩放
-	bool onZoomPerspective(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us, double dy = 0.0, bool offsetCenter = true);
-private:
-	std::atomic_bool _bFlyTo;
-	osg::Vec3d _flyToPos;
-	osg::Vec3d _startPos;
-	double _angle;
-	double _lastLength;
-	osg::Timer_t _lastTime;
-	bool _bLeftDown; // roam 按下时的左键
-	int _lastFrame;
-	
-	osg::Vec2i _leftMosueDownPos;
-	osg::Vec3d _lastPickedPos;
-
-	osg::Vec2d _lastPanMousePos;
-
-	
 public:
+	bool isOrthoCamera(const osg::Matrix& m);
+	bool switchToOrtho(osg::Camera* pCamera);
+	bool switchToPerspective(osg::Camera* pCamera);
 	OrbitManipulatorEx();
+
 	static float _fov;
 	static float _zNear;
+private:
 	int _width;
 	int _height;
 	osg::Camera* _pCamera;
 	osgViewer::Viewer* _pViewer;
-	//bool _bBtnDown;
-	//osg::Vec3d _lastPanMousePos;
-	//全图
-	void zoomE();
-	void zoomE(osg::Vec3d center, double radius);
-
-	// 窗口缩放
-	void zoomW(osg::Vec2i start, osg::Vec2i end);
-
-	// 相机定位
-	void flyTo(osg::Vec3d pos);
-
-	bool isOrthoCamera(const osg::Matrix& m);
-	bool switchToOrtho(osg::Camera* pCamera);
-	bool switchToPerspective(osg::Camera* pCamera);
- 
-
 	osg::Vec3d _direction;
- 
-
 };
 
